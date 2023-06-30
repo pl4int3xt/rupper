340ba43f688735f408583f08b38ca738821a4e7f
b3njah <benjahjp@gmail.com>
2023-06-30 21:34:57 +0300

    complete

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,25 @@ fn main() {
     }
 
     let hash: &String = &args[1];
-    let password: &String = &args[2];
+    let password_file: &String = &args[2];
     let mut attempts: i32 = 1;
 
     println!("Attempting to crack {}", hash);
+    let password_list: Result<File, Error> = File::open(password_file).unwrap();
+    let reader: BufReader<File> = BufReader::new(password_list);
+
+    for line in reader.lines(){
+        let line: String = line.unwrap();
+        let password: Vec<u8> = line.trim().to_owned().into_bytes();
+        let password_hash: String = format!("{:x}", Sha256::digest(&password));
+        println!("[{}] {} == {}", attempts, std::str::from_utf8(&password).unwrap(), password_hash);
+
+        if &password_hash == hash {
+            println!("Password found after {} attempts! {} hashes to {}!", attempts, std::str::from_utf8(&password).unwrap(), password_hash);
+        }
+
+        attempts +=1;
+    }
+
+    println!("Password hash not found");
 }
