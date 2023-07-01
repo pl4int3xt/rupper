c9b6c82911bbcf5d45b4ec766a7978977d3afa37
b3njah <benjahjp@gmail.com>
2023-07-01 21:47:43 +0300

    md5

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use sha2::{Sha256, Digest};
+use md5::{Md5, Digest};
 use std::process::exit;
 
 fn main() {
-    let args: Vec<String> = env::args.collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
         println!("Invalid amount of arguments");
         println!("Example: cargo run <sha256 hash>");
@@ -17,7 +18,7 @@ fn main() {
     let mut attempts: i32 = 1;
 
     println!("Attempting to crack {}", hash);
-    let password_list: Result<File, Error> = File::open(password_file).unwrap();
+    let password_list = File::open(password_file).unwrap();
     let reader: BufReader<File> = BufReader::new(password_list);
 
     for line in reader.lines(){
@@ -28,10 +29,11 @@ fn main() {
 
         if &password_hash == hash {
             println!("Password found after {} attempts! {} hashes to {}!", attempts, std::str::from_utf8(&password).unwrap(), password_hash);
+            break;
         }
 
         attempts +=1;
     }
-
-    println!("Password hash not found");
+    
+    println!("Not found");
 }
