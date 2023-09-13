287e1963db7388ec6f3be551b33900b31b34e34d
b3njah <benjahjp@gmail.com>
2023-09-13 18:17:44 +0300

    crack password

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::process::exit;
 
 mod utils;
@@ -13,7 +11,7 @@ fn main() {
 
     if args.len() != 5 {
         println!("Invalid amount of arguments");
-        println!("Example: cargo run <algorithm> <sha256 hash> <pass list> <verbose>");
+        println!("Example: cargo run <algorithm> <sha256 hash> <pass list> <verbose> <threads>");
         println!("Algorithms: <sha1> <sha256> <md2> <md4> <md5>");
         exit(1);
     }
@@ -22,36 +20,12 @@ fn main() {
     let hash: &String = &args[2];
     let password_file: &String = &args[3];
     let verbose: &String = &args[4];
-    let mut attempts: i32 = 1;
+    // let threads: &i32 = &args[5].parse::<i32>().unwrap();
+
+    // let mut handles: Vec<i32> = vec![];
 
     println!("Attempting to crack {}", hash);
-    let password_list = File::open(password_file).unwrap();
-    let reader: BufReader<File> = BufReader::new(password_list);
 
-    for line in reader.lines(){
-        let line: String = line.unwrap();
-        let password: Vec<u8> = line.trim().to_owned().into_bytes();
-        let hash_algorithm: algorithm::algorithm::HashAlgorithm = handlers::handlers::assign_algorithm_type(hash_type);
-        let password_hash: String = handlers::handlers::digest(&hash_algorithm, &password);
-        
-        if password_hash == "Invalid algorithm"{
-            println!("Invalid algorithm");
-            break;
-        }
-
-        utils::utils::check_verbose(verbose, &attempts, &password, &password_hash); 
-    
-        if &password_hash == hash {
-            println!("Password found");
-            println!("-----------------------------------------------------------------------------------");
-            println!("Attempts [{}]", attempts);
-            println!("Password [{}]", std::str::from_utf8(&password).unwrap());
-            println!("Hash [{}]", password_hash);
-            println!("-----------------------------------------------------------------------------------");
-            break;
-        }
-
-        attempts +=1;
-    }
+    utils::utils::crack_password(password_file, hash, verbose, hash_type)
 
 }
--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,4 +1,8 @@
 use text_to_ascii_art::convert;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use crate::algorithm::algorithm::HashAlgorithm;
+use crate::handlers::handlers::{assign_algorithm_type, digest};
 
 pub fn banner(banner: &str){
     match convert(banner.to_string()) {
@@ -17,5 +21,36 @@ pub fn check_verbose(verbose: &str, attempts: &i32, password: &Vec<u8>, password
         "true" => println!("[{}] [{}] == [{}]", attempts, std::str::from_utf8(&password).unwrap(), password_hash),
         "false" => {},
         _ => println!("Invalid verbose mode")
+    }
+}
+
+pub fn crack_password(password_file: &String, hash:&String, verbose: &str, hash_type: &str){
+    let mut attempts: i32 = 1;
+    let password_list = File::open(password_file).unwrap();
+    let reader: BufReader<File> = BufReader::new(password_list);
+    for line in reader.lines(){
+        let line: String = line.unwrap();
+        let password: Vec<u8> = line.trim().to_owned().into_bytes();
+        let hash_algorithm: HashAlgorithm = assign_algorithm_type(hash_type);
+        let password_hash: String = digest(&hash_algorithm, &password);
+        
+        if password_hash == "Invalid algorithm"{
+            println!("Invalid algorithm");
+            break;
+        }
+
+        check_verbose(verbose, &attempts, &password, &password_hash); 
+    
+        if &password_hash == hash {
+            println!("Password found");
+            println!("-----------------------------------------------------------------------------------");
+            println!("Attempts [{}]", attempts);
+            println!("Password [{}]", std::str::from_utf8(&password).unwrap());
+            println!("Hash [{}]", password_hash);
+            println!("-----------------------------------------------------------------------------------");
+            break;
+        }
+
+        attempts +=1;
     }
 }
